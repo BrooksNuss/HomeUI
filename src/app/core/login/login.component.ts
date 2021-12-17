c2778af66720bd26fa4b36b13a9a72a22c347d9b
Brooks Nuss <bnussjr@gmail.com>
2021-12-17 03:07:33 -0500

    Implemented mfa code component

--- src/app/core/login/login.component.ts
+++ src/app/core/login/login.component.ts
@@ -1,5 +1,7 @@
-import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { Subscription } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 import { LoginModalComponent } from './login-modal/login-modal.component';
 
 @Component({
@@ -7,12 +9,25 @@ import { LoginModalComponent } from './login-modal/login-modal.component';
 	templateUrl: './login.component.html',
 	styleUrls: ['./login.component.scss'],
 })
-export class LoginComponent implements AfterViewInit, OnInit {
+export class LoginComponent implements AfterViewInit, OnInit, OnDestroy {
 	private dialog: MatDialogRef<LoginModalComponent>;
+	private loginSub: Subscription;
 
-	constructor(private dialogService: MatDialog) {}
+	constructor(private dialogService: MatDialog, private authService: AuthService) {}
 
-	ngOnInit(): void {}
+	ngOnInit(): void {
+		this.authService.isLoggedIn.subscribe(next => {
+			if (next && this.dialog) {
+				this.dialog.close();
+			}
+		})
+	}
+
+	ngOnDestroy(): void {
+		if (this.loginSub) {
+			this.loginSub.unsubscribe();
+		}
+	}
 
 	ngAfterViewInit(): void {
 		this.dialog = this.dialogService.open(LoginModalComponent, {
--- src/app/core/login/mfa-code/mfa-code.component.ts
+++ src/app/core/login/mfa-code/mfa-code.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Injector, OnInit } from '@angular/core';
+import { LoginModalStep } from '../abstract/LoginModalStep';
 
 @Component({
 	selector: 'app-mfa-code',
 	templateUrl: './mfa-code.component.html',
 	styleUrls: ['./mfa-code.component.scss']
 })
-export class MfaCodeComponent implements OnInit {
+export class MfaCodeComponent extends LoginModalStep implements OnInit {
+	code = '';
+	validCode = true;
 
-	constructor() { }
+	constructor(injector: Injector) {
+		super(injector, 'mfa-login');
+	}
 
 	ngOnInit(): void {
 	}
 
+	async submitCode(): Promise<void> {
+		const valid = await this.authService.submitMfaChallenge(this.code);
+		if (valid) {
+			this.router.navigate(['home']);
+		}
+	}
 }
