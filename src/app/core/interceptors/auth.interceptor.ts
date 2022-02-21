a1d77a19f1a00a51faf0ef100efe61e2c2149c89
Brooks Nuss <bnussjr@gmail.com>
2022-02-21 01:41:47 -0500

    Rewriting interceptor

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -6,7 +6,7 @@ import {
 	HttpInterceptor,
 	HttpHeaders
 } from '@angular/common/http';
-import { firstValueFrom, from, Observable } from 'rxjs';
+import { firstValueFrom, from, Observable, switchMap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable()
@@ -15,16 +15,24 @@ export class AuthInterceptor implements HttpInterceptor {
 	constructor(private authService: AuthService) {}
 
 	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<unknown>> {
-		return from(this.handle(request, next));
+		return from(this.authService.getAuthToken())
+			.pipe(switchMap(token => {
+				const authReq = request.clone({
+					headers: new HttpHeaders({
+						'Authorization': token
+					})
+				});
+				return next.handle(authReq);
+			}));
 	}
 
-	async handle(request: HttpRequest<any>, next: HttpHandler) {
-		const token = await this.authService.getAuthToken();
-		const authReq = request.clone({
-			headers: new HttpHeaders({
-				'Authorization': token
-			})
-		});
-		return firstValueFrom(next.handle(authReq));
-	}
+	// async handle(request: HttpRequest<any>, next: HttpHandler) {
+	// 	const token = await this.authService.getAuthToken();
+	// 	const authReq = request.clone({
+	// 		headers: new HttpHeaders({
+	// 			'Authorization': token
+	// 		})
+	// 	});
+	// 	return firstValueFrom(next.handle(authReq));
+	// }
 }
