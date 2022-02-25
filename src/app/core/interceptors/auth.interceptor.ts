7b3b892ba684aa60ada19598e8fc7d4ed25b56eb
Brooks Nuss <bnussjr@gmail.com>
2022-02-25 17:32:42 -0500

    Added enter key listener to login flow

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -6,7 +6,7 @@ import {
 	HttpInterceptor,
 	HttpHeaders
 } from '@angular/common/http';
-import { firstValueFrom, from, Observable, switchMap } from 'rxjs';
+import { from, Observable, switchMap } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable()
@@ -25,14 +25,4 @@ export class AuthInterceptor implements HttpInterceptor {
 				return next.handle(authReq);
 			}));
 	}
-
-	// async handle(request: HttpRequest<any>, next: HttpHandler) {
-	// 	const token = await this.authService.getAuthToken();
-	// 	const authReq = request.clone({
-	// 		headers: new HttpHeaders({
-	// 			'Authorization': token
-	// 		})
-	// 	});
-	// 	return firstValueFrom(next.handle(authReq));
-	// }
 }
